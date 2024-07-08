9c4b19c3a3726f640a57e4d8038d2037ff6f2ede
Jiwon Hwang <83958344+Jiwon-0326@users.noreply.github.com>
2024-07-08 19:28:57 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,5 +1,3 @@
 def solution(myString, pat):
     answer = myString.replace('A', 'a').replace('B', 'A').replace('a','B')
-    if answer.find(pat) >= 0 :
-        return 1
-    else : return 0
+    return int(pat in answer)
