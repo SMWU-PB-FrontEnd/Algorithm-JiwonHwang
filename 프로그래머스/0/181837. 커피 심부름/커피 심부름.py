57664c6081703ba035f066b8042181468996712a
Jiwon Hwang <83958344+Jiwon-0326@users.noreply.github.com>
2024-07-29 13:09:55 +0900

    [level 0] Title: 커피 심부름, Time: 0.09 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,10 @@
+def solution(order):
+    answer = 0
+    for x in order :
+        if 'americano' in x :
+            answer += 4500
+        elif 'latte' in x :
+            answer += 5000
+        else :
+            answer += 4500
+    return answer
